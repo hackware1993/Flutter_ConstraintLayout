11b1a6d711778a7c2b7dd4f06cb86ede616cf242
hackware1993 <hackware1993@gmail.com>
2022-04-12 23:30:53 +0800

    update example

--- example/complex_list.dart
+++ example/complex_list.dart
@@ -22,85 +22,95 @@ class ComplexListExample extends StatelessWidget {
       home: Scaffold(
         body: ListView.builder(
           itemBuilder: (context, index) {
-            return Container(
-              child: ConstraintLayout(
-                children: [
-                  Image.asset(
-                    'assets/test.png',
-                    fit: BoxFit.fill,
-                  ).applyConstraint(
-                    width: 100,
-                    height: wrapContent,
-                    centerLeftTo: parent,
-                  ),
-                  Image.asset(
-                    'assets/test2.png',
-                    fit: BoxFit.fill,
-                  ).applyConstraint(
-                    width: 150,
-                    height: wrapContent,
-                    centerRightTo: parent,
-                  ),
-                  Text(
-                    'topLeft $index',
-                    style: style,
-                  ).applyConstraint(
-                    topLeftTo: parent,
-                  ),
-                  Text(
-                    'topCenter $index',
-                    style: style,
-                  ).applyConstraint(
-                    topCenterTo: parent,
-                  ),
-                  Text(
-                    'topRight $index',
-                    style: style,
-                  ).applyConstraint(
-                    topRightTo: parent,
-                  ),
-                  Text(
-                    'centerLeft $index',
-                    style: style,
-                  ).applyConstraint(
-                    centerLeftTo: parent,
-                  ),
-                  Text(
-                    'center $index',
-                    style: style,
-                  ).applyConstraint(
-                    centerTo: parent,
-                  ),
-                  Text(
-                    'centerRight $index',
-                    style: style,
-                  ).applyConstraint(
-                    centerRightTo: parent,
-                  ),
-                  Text(
-                    'bottomLeft $index',
-                    style: style,
-                  ).applyConstraint(
-                    bottomLeftTo: parent,
-                  ),
-                  Text(
-                    'bottomCenter $index',
-                    style: style,
-                  ).applyConstraint(
-                    bottomCenterTo: parent,
-                  ),
-                  Text(
-                    'bottomRight $index',
-                    style: style,
-                  ).applyConstraint(
-                    bottomRightTo: parent,
-                  )
-                ],
-              ),
-              color: colors[index % 6],
-              margin: const EdgeInsets.only(
-                top: 10,
-              ),
+            if (index == 0) {
+              return const Text(
+                'Very complex item view can also achieve full frame',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              );
+            }
+            return ConstraintLayout(
+              children: [
+                Container(
+                  color: colors[index % 6],
+                ).applyConstraint(
+                  width: matchParent,
+                  height: matchParent,
+                ),
+                Image.asset(
+                  'assets/test.png',
+                  fit: BoxFit.fill,
+                ).applyConstraint(
+                  width: 100,
+                  height: wrapContent,
+                  centerLeftTo: parent,
+                ),
+                Image.asset(
+                  'assets/test2.png',
+                  fit: BoxFit.fill,
+                ).applyConstraint(
+                  width: 150,
+                  height: wrapContent,
+                  centerRightTo: parent,
+                ),
+                Text(
+                  'topLeft $index',
+                  style: style,
+                ).applyConstraint(
+                  topLeftTo: parent,
+                ),
+                Text(
+                  'topCenter $index',
+                  style: style,
+                ).applyConstraint(
+                  topCenterTo: parent,
+                ),
+                Text(
+                  'topRight $index',
+                  style: style,
+                ).applyConstraint(
+                  topRightTo: parent,
+                ),
+                Text(
+                  'centerLeft $index',
+                  style: style,
+                ).applyConstraint(
+                  centerLeftTo: parent,
+                ),
+                Text(
+                  'center $index',
+                  style: style,
+                ).applyConstraint(
+                  centerTo: parent,
+                ),
+                Text(
+                  'centerRight $index',
+                  style: style,
+                ).applyConstraint(
+                  centerRightTo: parent,
+                ),
+                Text(
+                  'bottomLeft $index',
+                  style: style,
+                ).applyConstraint(
+                  bottomLeftTo: parent,
+                ),
+                Text(
+                  'bottomCenter $index',
+                  style: style,
+                ).applyConstraint(
+                  bottomCenterTo: parent,
+                ),
+                Text(
+                  'bottomRight $index',
+                  style: style,
+                ).applyConstraint(
+                  bottomRightTo: parent,
+                )
+              ],
             );
           },
           itemCount: 10000,
