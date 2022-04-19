b7972ab1906cc0cff9e67df138309b07b739e069
fbchen <chen.fangbing@tslsmart.com>
2022-04-19 10:04:12 +0800

    update example

--- example/wrapper_constraints.dart
+++ example/wrapper_constraints.dart
@@ -6,11 +6,11 @@ import 'custom_app_bar.dart';
 class WrapperConstraintsExample extends StatelessWidget {
   const WrapperConstraintsExample({Key? key}) : super(key: key);
 
-  Widget item(String text) {
+  Widget item(String text, [Color color = Colors.redAccent]) {
     return Container(
       width: 100,
       height: 100,
-      color: Colors.redAccent,
+      color: color,
       alignment: Alignment.center,
       child: FittedBox(
         child: Text(text),
@@ -47,6 +47,30 @@ class WrapperConstraintsExample extends StatelessWidget {
               right: 200,
             ),
           ),
+          item('outTopLeftTo').applyConstraint(
+            outTopLeftTo: rId(1),
+          ),
+          item('outTopCenterTo').applyConstraint(
+            outTopCenterTo: rId(1),
+          ),
+          item('outTopRightTo').applyConstraint(
+            outTopRightTo: rId(1),
+          ),
+          item('outCenterLeftTo').applyConstraint(
+            outCenterLeftTo: rId(1),
+          ),
+          item('outCenterRightTo').applyConstraint(
+            outCenterRightTo: rId(1),
+          ),
+          item('outBottomLeftTo').applyConstraint(
+            outBottomLeftTo: rId(1),
+          ),
+          item('outBottomCenterTo').applyConstraint(
+            outBottomCenterTo: rId(1),
+          ),
+          item('outBottomRightTo').applyConstraint(
+            outBottomRightTo: rId(1),
+          ),
           item('centerTopLeftTo').applyConstraint(
             centerTopLeftTo: rId(0),
           ),
@@ -71,29 +95,32 @@ class WrapperConstraintsExample extends StatelessWidget {
           item('centerBottomRightTo').applyConstraint(
             centerBottomRightTo: rId(0),
           ),
-          item('outTopLeftTo').applyConstraint(
-            outTopLeftTo: rId(1),
+          item('topLeftTo', Colors.green).applyConstraint(
+            topLeftTo: rId(1),
           ),
-          item('outTopCenterTo').applyConstraint(
-            outTopCenterTo: rId(1),
+          item('topCenterTo', Colors.green).applyConstraint(
+            topCenterTo: rId(1),
           ),
-          item('outTopRightTo').applyConstraint(
-            outTopRightTo: rId(1),
+          item('topRightTo', Colors.green).applyConstraint(
+            topRightTo: rId(1),
           ),
-          item('outCenterLeftTo').applyConstraint(
-            outCenterLeftTo: rId(1),
+          item('centerLeftTo', Colors.green).applyConstraint(
+            centerLeftTo: rId(1),
           ),
-          item('outCenterRightTo').applyConstraint(
-            outCenterRightTo: rId(1),
+          item('centerTo', Colors.green).applyConstraint(
+            centerTo: rId(1),
           ),
-          item('outBottomLeftTo').applyConstraint(
-            outBottomLeftTo: rId(1),
+          item('centerRightTo', Colors.green).applyConstraint(
+            centerRightTo: rId(1),
           ),
-          item('outBottomCenterTo').applyConstraint(
-            outBottomCenterTo: rId(1),
+          item('bottomLeftTo', Colors.green).applyConstraint(
+            bottomLeftTo: rId(1),
           ),
-          item('outBottomRightTo').applyConstraint(
-            outBottomRightTo: rId(1),
+          item('bottomCenterTo', Colors.green).applyConstraint(
+            bottomCenterTo: rId(1),
+          ),
+          item('bottomRightTo', Colors.green).applyConstraint(
+            bottomRightTo: rId(1),
           ),
         ],
       ),
