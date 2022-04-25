c56e0293ea493dfd2365e35eed18a8df10c419ce
fbchen <chen.fangbing@tslsmart.com>
2022-04-25 13:35:55 +0800

    update example

--- example/circle_position.dart
+++ example/circle_position.dart
@@ -1,10 +1,50 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_constraintlayout/src/constraint_layout.dart';
 
 import 'custom_app_bar.dart';
 
-class CirclePositionExample extends StatelessWidget {
+class CirclePositionExample extends StatefulWidget {
   const CirclePositionExample({Key? key}) : super(key: key);
+
+  @override
+  State createState() => CirclePositionExampleState();
+}
+
+class CirclePositionExampleState extends State<CirclePositionExample> {
+  late Timer timer;
+  late int hour;
+  late int minute;
+  late int second;
+
+  double centerTranslateX = 0;
+  double centerTranslateY = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    calculateClockAngle();
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      calculateClockAngle();
+    });
+  }
+
+  void calculateClockAngle() {
+    setState(() {
+      DateTime now = DateTime.now();
+      hour = now.hour;
+      minute = now.minute;
+      second = now.second;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,25 +55,113 @@ class CirclePositionExample extends StatelessWidget {
       ),
       body: ConstraintLayout(
         children: [
-          Container(
-            color: Colors.redAccent,
+          GestureDetector(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(1000),
+                ),
+              ),
+            ),
+            onPanUpdate: (details) {
+              setState(() {
+                centerTranslateX += details.delta.dx;
+                centerTranslateY += details.delta.dy;
+              });
+            },
           ).applyConstraint(
-            width: 100,
-            height: 100,
+            width: 20,
+            height: 20,
             centerTo: parent,
+            zIndex: 100,
+            translate: Offset(centerTranslateX, centerTranslateY),
+            translateConstraint: true,
           ),
-          for (int angle = 0; angle <= 360; angle += 30)
+          for (int i = 0; i < 12; i++)
             Container(
-              color: Colors.yellow,
+              alignment: Alignment.center,
+              child: Text(
+                '${i + 1}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
             ).applyConstraint(
               width: 50,
               height: 50,
               centerTo: rId(0),
               translate: circleTranslate(
                 radius: 200,
-                angle: angle,
+                angle: (i + 1) * 30,
               ),
             ),
+          for (int i = 0; i < 60; i++)
+            Transform.rotate(
+              angle: pi + pi * (i * 6 / 180),
+              child: Container(
+                color: Colors.grey,
+                margin: const EdgeInsets.only(
+                  top: 405,
+                ),
+              ),
+            ).applyConstraint(
+              width: 1,
+              height: 415,
+              centerTo: rId(0),
+              visibility: i % 5 == 0 ? gone : visible,
+            ),
+          Transform.rotate(
+            angle: pi + pi * (hour * 30 / 180),
+            alignment: Alignment.topCenter,
+            child: Container(
+              color: Colors.green,
+            ),
+          ).applyConstraint(
+            width: 5,
+            height: 80,
+            centerTo: rId(0),
+            translate: const Offset(0, 0.5),
+            percentageTranslate: true,
+          ),
+          Transform.rotate(
+            angle: pi + pi * (minute * 6 / 180),
+            alignment: Alignment.topCenter,
+            child: Container(
+              color: Colors.pink,
+            ),
+          ).applyConstraint(
+            width: 5,
+            height: 120,
+            centerTo: rId(0),
+            translate: const Offset(0, 0.5),
+            percentageTranslate: true,
+          ),
+          Transform.rotate(
+            angle: pi + pi * (second * 6 / 180),
+            alignment: Alignment.topCenter,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ).applyConstraint(
+            width: 5,
+            height: 180,
+            centerTo: rId(0),
+            translate: const Offset(0, 0.5),
+            percentageTranslate: true,
+          ),
+          Text(
+            '$hour:$minute:$second',
+            style: const TextStyle(
+              fontSize: 40,
+            ),
+          ).applyConstraint(
+            outTopCenterTo: rId(0),
+            margin: const EdgeInsets.only(
+              bottom: 250,
+            ),
+          )
         ],
       ),
     );
