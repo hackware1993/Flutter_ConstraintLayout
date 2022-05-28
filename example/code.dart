865d5d3d1d477ff34a25473f6636be660e85e07a
hackware <cfb1993@163.com>
2022-05-28 08:51:23 +0800

    update example

--- example/code.dart
+++ example/code.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_constraintlayout/flutter_constraintlayout.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 import 'custom_app_bar.dart';
 
@@ -42,37 +43,36 @@ class CodeViewState extends State<CodeViewWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: ConstraintLayout(
-        children: [
-          if (loading)
-            const CircularProgressIndicator().applyConstraint(
-              size: 30,
-              centerTo: parent,
-            ),
-          if (!loading && code == null)
-            const Text('Code loading failed').applyConstraint(
-              centerTo: parent,
-            ),
-          if (code != null)
-            SingleChildScrollView(
-              child: Padding(
-                child: Text(
-                  code ?? '',
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
-                padding: const EdgeInsets.only(
-                  left: 20,
-                  top: 20,
+      body: ConstraintLayout().open(() {
+        if (loading) {
+          const CircularProgressIndicator().applyConstraint(
+            size: 30,
+            centerTo: parent,
+          );
+        } else if (code == null) {
+          const Text('Code loading failed').applyConstraint(
+            centerTo: parent,
+          );
+        } else {
+          SingleChildScrollView(
+            child: Padding(
+              child: Text(
+                code ?? '',
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
                 ),
               ),
-            ).applyConstraint(
-              size: matchParent,
-            )
-        ],
-      ),
+              padding: const EdgeInsets.only(
+                left: 20,
+                top: 20,
+              ),
+            ),
+          ).applyConstraint(
+            size: matchParent,
+          );
+        }
+      }),
     );
   }
 }
