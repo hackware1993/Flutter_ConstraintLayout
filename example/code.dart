ee298e8afa42319bcaa336389eccbced937d5c49
hackware <cfb1993@163.com>
2022-05-28 08:53:26 +0800

    update example

--- example/code.dart
+++ example/code.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_constraintlayout/flutter_constraintlayout.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 
 import 'custom_app_bar.dart';
 
